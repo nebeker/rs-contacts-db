0155d8608815d3276e25e8a4d67a79221f3d8a44
Nuno Nebeker <nebeker@users.noreply.github.com>
2024-05-04 19:09:36 +0100

    Create contact.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,15 @@ use rs_contacts_db::*;
 fn main() {
     use self::schema::contacts::dsl::*;
 
+    let new_name = "Alice";
+    let new_email = "alice@example.com";
+
     let connection = &mut establish_connection();
+
+    let mut created_contact = create_contact(connection, new_name, new_email);
+
+    println!("Created new contact with ID {}", created_contact.id);
+
     let results = contacts
         .filter(active.eq(true))
         .limit(5)
