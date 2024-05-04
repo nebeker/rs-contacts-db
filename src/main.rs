c1f3b7bbe68a64ddd1da2f22ca97f80e9d50ef87
Nuno Nebeker <nebeker@users.noreply.github.com>
2024-05-04 18:42:53 +0100

    Read table.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use self::models::*;
+use diesel::prelude::*;
+use rs_contacts_db::*;
+
 fn main() {
-    println!("Hello, world!");
+    use self::schema::contacts::dsl::*;
+
+    let connection = &mut establish_connection();
+    let results = contacts
+        .filter(active.eq(true))
+        .limit(5)
+        .select(Contact::as_select())
+        .load(connection)
+        .expect("Error loading contacts");
+
+    println!("Displaying {} contacts", results.len());
+    for contact in results {
+        println!("{}", contact.name);
+        println!("-----------\n");
+        println!("{}", contact.email);
+    }
 }
